f86e7d3318938b29c9e3a514a60dd987bea32581
Konstantin Stepakh <1ordcommander@duck.com>
2021-12-09 18:32:22 +0200

    Uncomment price

--- src/modules/yield-farming/components/pool-stats/index.tsx
+++ src/modules/yield-farming/components/pool-stats/index.tsx
@@ -90,7 +90,7 @@ const PoolStats: React.FC<Props> = ({ className }) => {
       </div>
 
       {/* ToDo: Uncomment after SushiSwap deployment */}
-      {/* <div className="card p-24">
+       <div className="card p-24">
         <div className="flex flow-row justify-space-between full-height">
           <Text type="lb2" weight="semibold" color="primary" className={cn(s.label, 'mb-48')}>
             {LeagueToken.symbol} Price
@@ -106,7 +106,7 @@ const PoolStats: React.FC<Props> = ({ className }) => {
             </ExternalLink>
           </div>
         </div>
-      </div> */}
+      </div>
 
       {!isEnded && (
         <div className="card p-24">
