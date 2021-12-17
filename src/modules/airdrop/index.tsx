92a27fa07eb0103a6a59870558499b8259cef34f
deluxart <deluxart3@gmail.com>
2021-12-17 10:03:43 +0200

    Add cup animation

--- src/modules/airdrop/index.tsx
+++ src/modules/airdrop/index.tsx
@@ -1,18 +1,25 @@
 import React, { FC } from 'react';
+import Lottie from 'lottie-react';
 
-import styles from './airdrop.module.scss';
+// import Tooltip from '../../components/antd/tooltip';
 import Grid from '../../components/custom/grid';
 import Icon from '../../components/custom/icon';
-import Tooltip from '../../components/antd/tooltip';
-// import { Grid } from 'antd';
+import cupSvg from '../../resources/svg/cup_transparent.svg';
+import cupWaveAnimation from './animations/waves.json';
+
+import styles from './airdrop.module.scss';
+
+const progressPercent = 50;
 
 const Airdrop: FC = () => {
   return (
     <div className={styles.container}>
       <div className={styles.general__info}>
         <h1>Airdrop reward</h1>
-        <p>
-          You may have received claimable token rewards from the LeagueDAO Airdrop. Claiming your airdrop will forfeit a portion of your balance. Your total claimable amount will rise whenever someone forfeits a portion of their reward.
+        <p className="mb-48">
+          You may have received claimable token rewards from the LeagueDAO Airdrop. Claiming your airdrop will forfeit a
+          portion of your balance. Your total claimable amount will rise whenever someone forfeits a portion of their
+          reward.
         </p>
       </div>
       <Grid flow="col" colsTemplate="6fr 3fr" gap={30} width="100%">
@@ -96,14 +103,20 @@ const Airdrop: FC = () => {
             </div>
             <div className={styles.airdrop__info__week_counter}>WEEK 15/100</div>
           </Grid>
-          <Icon
-            width={386}
-            height={400}
-            name="png/footballScene"
-            className={styles.footballer}
-          />
+          {/*<Icon width={386} height={400} name="png/footballScene" className={styles.footballer} />*/}
         </Grid>
-
+        <div className={styles.airdrop__info__container}>
+          <div className={styles.airdrop__animateBlock}>
+            <div className={styles.cupBlock}>
+              <img src={cupSvg} alt="" />
+              <Lottie
+                animationData={cupWaveAnimation}
+                style={{ transform: `translateY(calc(-${progressPercent}% - -10px))` }}
+                className={styles.waveAnimation}
+              />
+            </div>
+          </div>
+        </div>
       </Grid>
       {/*<div className={styles.total__info__container}>*/}
       {/*  <div>1</div>*/}
